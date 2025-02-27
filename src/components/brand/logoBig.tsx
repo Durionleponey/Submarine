bc4143f08a146d4a76be33895de67c79d6316aab
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-02-27 14:27:18 +0100

    front Authentification

---
+++ src/components/brand/logoBig.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+export default function LogoBig() {
+    return (
+        <img
+            src="/img/submarineLogo.png"
+            alt="logo"
+            style={{
+                width: "100%",
+                maxWidth: "200px",
+                height: "auto",
+                display: "block",
+                margin: "0 auto"
+            }}
+        />
+    );
+}

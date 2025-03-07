9b3283a7aa40a211028247cbf080effd2c1129da
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-03-07 09:12:46 +0100

    password encryttion for users with bcryptjs

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import {UsersRepository} from "./users.repository";
+import * as bcrypt from 'bcryptjs'
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
-  create(createUserInput: CreateUserInput) {
+  async create(createUserInput: CreateUserInput) {
     //console.log("frffrrfe", createUserInput);
-    return this.userRepository.create(createUserInput);
+    return this.userRepository.create({
+      ...createUserInput,
+      password: await bcrypt.hash(createUserInput.password, 10)//bcrypt is a library
+    });
   }
 
   async findAll() {

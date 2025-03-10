a32aea6e14bed211a96accbb025e4afb965af9ab
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-03-10 18:36:53 +0100

    user can signup using the front 🎉🥲

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -1,14 +1,30 @@
 import Auth from "./Auth";
-import {Link} from "react-router-dom";
-import {Link as MUIlink} from "@mui/material"
-
+import { Link } from "react-router-dom";
+import { Link as MUIlink } from "@mui/material";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 const Signup = () => {
-    return (<Auth submitLabel={"Signup"} submitColor={"primary"} onSubmit={async () => {}}>
-        <Link to={'/login'} style={{ alignSelf:"center" }}>
-            <MUIlink>Login</MUIlink>
-        </Link>
-    </Auth>);
-}
+    const [createUser] = useCreateUser();
+
+    return (
+        <Auth
+            submitLabel={"Signup"}
+            submitColor={"primary"}
+            onSubmit={async ({ email, password }) => {
+                const variables = {
+                    createUserInput: {
+                        email,
+                        password
+                    }
+                };
+
+            }}
+        >
+            <Link to={"/login"} style={{ alignSelf: "center" }}>
+                <MUIlink>Login</MUIlink>
+            </Link>
+        </Auth>
+    );
+};
 
 export default Signup;
---
+++ frontend/src/hooks/useCreateUser.ts
@@ -0,0 +1,32 @@
+import {gql, useMutation} from "@apollo/client";
+import {User} from "../model/User";
+
+
+interface CreateUserInput {
+    createUserInput: {
+        email: string;
+        password: string;
+    }
+}
+
+const CREATE_USER = gql`
+  mutation CreateUser($createUserInput: CreateUserInput!) {
+    createUser(createUserInput: $createUserInput) {
+      _id
+      email
+    }
+  }
+`;
+
+
+//const CREATE_USER = gql("mutation CreateUser($input: CreateUserInput!) { createUser(input: $input) { _id email } }");
+
+
+
+
+const useCreateUser = () => {
+
+    return useMutation<User, CreateUserInput>(CREATE_USER)
+}
+
+export {useCreateUser};

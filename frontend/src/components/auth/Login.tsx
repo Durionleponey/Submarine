37158792d165cafba6a8be2caf867974b2c75ad7
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-03-21 16:29:34 +0100

    user can not login using the front!

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,11 +1,13 @@
 import Auth from "./Auth";
 import { Link } from "react-router-dom";
 import {Link as MUIlink} from "@mui/material"
+import useLogin from "../../hooks/useLogin";
 
 
 const Login = () => {
+    const { login, error } = useLogin()
     return (
-        <Auth submitLabel="Login" submitColor={"secondary"} onSubmit={async () => {}}>
+        <Auth submitLabel="Login" submitColor={"secondary"} onSubmit={(request) => login(request)}>
             <Link to="/signup" style={{alignSelf:"center"}}>
                 <MUIlink >
                     Signup

8902b5becc601666f255cc140841cab1591bdd1b
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-03-28 15:51:38 +0100

    user is redirected when he is login and have a signup succes message + no more signup form

--- frontend/src/components/auth/Auth.tsx
+++ frontend/src/components/auth/Auth.tsx
@@ -1,6 +1,8 @@
-import {Button, Stack, TextField} from "@mui/material";
+import {Button, Stack, TextField, Typography} from "@mui/material";
 import LogoBig from "../brand/logoBig"
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {useGetMe} from "../../hooks/useGetMe";
+import {useNavigate} from "react-router-dom";
 
 interface AuthProps {
     submitLabel: string
@@ -8,21 +10,38 @@ interface AuthProps {
     onSubmit: (credentials: { email:string; password: string;}) => Promise<void>;
     children: React.ReactNode;
     error?: string;
+    success?: boolean;
 }
 
 
-const Auth = ({submitLabel, submitColor, onSubmit, children, error}:AuthProps) => {
+const Auth = ({submitLabel, submitColor, onSubmit, children, error, success}:AuthProps) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const {data} = useGetMe();
+    const navigate = useNavigate();
+
+    useEffect(() =>{
+        if (data) {
+            navigate("/");
+
+        }
+    }, [data,navigate]);//i have to put navigate in the array because is good practice
 
     return(
         <Stack spacing={5} sx={
             { height: "100vh", maxWidth: {xs: '70%', md: '30%'}, margin: "0 auto", justifyContent: "center"}}>
             <LogoBig/>
-            <TextField type="mail" label="Email" value={email} error={!!error} helperText={error} onChange={(event) => setEmail(event.target.value)}/>
-            <TextField type="password" label="Password" value={password}  error={!!error} helperText={error} onChange={(event) => setPassword(event.target.value)}/>
-            <Button variant="outlined" color={submitColor} onClick={() => onSubmit({email, password})}>{submitLabel}</Button>
+            {!success &&<><TextField type="mail" label="Email" value={email} error={!!error} helperText={error} onChange={(event) => setEmail(event.target.value)}/>
+            <TextField type="password" label="Password" value={password} error={!!error} helperText={error} onChange={(event) => setPassword(event.target.value)}/>
+            <Button variant="outlined"  color={submitColor} onClick={() => onSubmit({email, password})}>{submitLabel}</Button></>}
+            <>{success && <>  <Typography
+                variant="h6"
+                align="center"
+                sx={{ color: "white", fontWeight: "bold" }}
+            >
+                You are successfully signup 💖 !
+            </Typography></>}</>
             { children }
         </Stack>
     )
--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -8,6 +8,7 @@ import {useState} from "react";
 const Signup = () => {
     const [createUser] = useCreateUser();
     const [isError, setIsError] = useState<undefined | string>();
+    const [isSucces, setIsSucces] = useState<undefined | boolean>();
 
 
     return (
@@ -28,19 +29,26 @@ const Signup = () => {
                         },
 
                     })
+                    setIsSucces(true);
+
+
                 }catch (error) {
                     //alert("Password not strong anought or email already use!")
                     setIsError("Password not strong anought, or bad email !")
+                    setIsSucces(false);
                 }
 
                 //console.log("Signup clicked", variables);
             }}
             error={isError}
+            success={isSucces}
+
 
         >
             <MUIlink component={Link} to="/login" style={{ alignSelf: "center" }}>
                 Login
             </MUIlink>
+
         </Auth>
     );
 };

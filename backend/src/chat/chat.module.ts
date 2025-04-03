f563a4f3a475c91f41227303f69796298a9f2b02
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-04-03 10:27:06 +0200

    fix chat module

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatResolver } from './chat.resolver';
 import {ChatRepository} from "./chat.repository";
+import { MongooseModule } from '@nestjs/mongoose';
+import { Chat, ChatSchema } from './entities/chat.entity';
+
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Chat.name, schema: ChatSchema }
+    ])
+  ],
   providers: [ChatResolver, ChatService, ChatRepository],
 })
 export class ChatModule {}

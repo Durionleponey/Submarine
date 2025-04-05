7b16b5442a98573834c427a22e5b7deaf18384a0
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-04-05 10:16:50 +0200

    adding codegen for sql interface import + refactoruseCreateUser to test

--- frontend/src/hooks/useCreateUser.ts
+++ frontend/src/hooks/useCreateUser.ts
@@ -1,24 +1,26 @@
 import {gql, useMutation} from "@apollo/client";
-import {User} from "../model/User";
+//import {User} from "../model/User";
+import {graphql} from "../gql";
+
 
 //this hook is for graphQL
 
 
-interface CreateUserInput {
+/*interface CreateUserInput {
     createUserInput: {
         email: string;
         password: string;
     }
-}
+}*/
 
-const CREATE_USER = gql`
+const CREATE_USER = graphql(`
   mutation CreateUser($createUserInput: CreateUserInput!) {
     createUser(createUserInput: $createUserInput) {
       _id
       email
     }
   }
-`;
+`);
 
 
 //const CREATE_USER = gql("mutation CreateUser($input: CreateUserInput!) { createUser(input: $input) { _id email } }");
@@ -26,9 +28,16 @@ const CREATE_USER = gql`
 
 
 
-const useCreateUser = () => {
+/*const useCreateUser = () => {
 
     return useMutation<User, CreateUserInput>(CREATE_USER)
+}
+
+export {useCreateUser};*/
+
+const useCreateUser = () => {
+
+    return useMutation(CREATE_USER)
 }
 
 export {useCreateUser};

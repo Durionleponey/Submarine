8c5b572c7b00975f93469ea3a6baa9aabbe2675f
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-04-08 10:02:09 +0200

    chat update dynamicly in the ui

--- frontend/src/gql/gql.ts
+++ frontend/src/gql/gql.ts
@@ -15,11 +15,13 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  */
 type Documents = {
     "\n    mutation CreateChat($createChatInput: CreateChatInput!) {\n        createChat(createChatInput: $createChatInput) {\n            _id\n            userId\n            isPrivate\n            userIds\n            name\n        }\n    }\n": typeof types.CreateChatDocument,
+    "\n    fragment ChatFragment on Chat {\n        _id\n        userId\n        isPrivate\n        userIds\n        name\n    }\n": typeof types.ChatFragmentFragmentDoc,
     "\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n": typeof types.CreateUserDocument,
     "\n    query Chats{\n        chatss {\n            _id\n            userId\n            isPrivate\n            userIds\n            name\n        }\n    }\n": typeof types.ChatsDocument,
 };
 const documents: Documents = {
     "\n    mutation CreateChat($createChatInput: CreateChatInput!) {\n        createChat(createChatInput: $createChatInput) {\n            _id\n            userId\n            isPrivate\n            userIds\n            name\n        }\n    }\n": types.CreateChatDocument,
+    "\n    fragment ChatFragment on Chat {\n        _id\n        userId\n        isPrivate\n        userIds\n        name\n    }\n": types.ChatFragmentFragmentDoc,
     "\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n": types.CreateUserDocument,
     "\n    query Chats{\n        chatss {\n            _id\n            userId\n            isPrivate\n            userIds\n            name\n        }\n    }\n": types.ChatsDocument,
 };
@@ -42,6 +44,10 @@ export function graphql(source: string): unknown;
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n    mutation CreateChat($createChatInput: CreateChatInput!) {\n        createChat(createChatInput: $createChatInput) {\n            _id\n            userId\n            isPrivate\n            userIds\n            name\n        }\n    }\n"): (typeof documents)["\n    mutation CreateChat($createChatInput: CreateChatInput!) {\n        createChat(createChatInput: $createChatInput) {\n            _id\n            userId\n            isPrivate\n            userIds\n            name\n        }\n    }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n    fragment ChatFragment on Chat {\n        _id\n        userId\n        isPrivate\n        userIds\n        name\n    }\n"): (typeof documents)["\n    fragment ChatFragment on Chat {\n        _id\n        userId\n        isPrivate\n        userIds\n        name\n    }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- frontend/src/hooks/useCreateChat.ts
+++ frontend/src/hooks/useCreateChat.ts
@@ -17,14 +17,39 @@ const createChatDocument = graphql(`
     }
 `);
 
+const ChatFragment = graphql(`
+    fragment ChatFragment on Chat {
+        _id
+        userId
+        isPrivate
+        userIds
+        name
+    }
+`);
 
 
 
+
+//i for update the cache so when we create a new chat it will be add directly in the ui but it is a mess i know
 
 const useCreateChat = () =>{
 
 
-    return useMutation(createChatDocument);
+    return useMutation(createChatDocument,{
+        update(cache, { data }){
+            cache.modify({
+                fields: {
+                    chatss(existingChats=[]) {
+                        const newChatRef = cache.writeFragment({
+                            data: data?.createChat,
+                            fragment: ChatFragment
+                        })
+                        return [...existingChats, newChatRef];
+                    }
+                }
+            })
+        },
+    });
 }
 
 
